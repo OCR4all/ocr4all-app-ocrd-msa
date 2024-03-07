770062590785fe1bf7a9a648751c1bac7759e40d
Herbert Baier <baier@itbaier.de>
2024-03-07 14:15:00 +0100

    launched/ocr-d msa server

---
+++ src/main/java/de/uniwuerzburg/zpd/ocr4all/application/ocrd/msa/Ocr4allAppOcrdMsaApplication.java
@@ -0,0 +1,34 @@
+/**
+ * File:     Ocr4allAppOcrdMsaApplication.java
+ * Package:  de.uniwuerzburg.zpd.ocr4all.application.ocrd.msa
+ * 
+ * Author:   Herbert Baier (herbert.baier@uni-wuerzburg.de)
+ * Date:     07.03.2024
+ */
+package de.uniwuerzburg.zpd.ocr4all.application.ocrd.msa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Triggers auto-configuration and component scanning and enables the ocr-d
+ * server.
+ *
+ * @author <a href="mailto:herbert.baier@uni-wuerzburg.de">Herbert Baier</a>
+ * @version 1.0
+ * @since 17
+ */
+@SpringBootApplication
+public class Ocr4allAppOcrdMsaApplication {
+
+	/**
+	 * The main method to start the orc-d server.
+	 * 
+	 * @param args The application arguments.
+	 * @since 17
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Ocr4allAppOcrdMsaApplication.class, args);
+	}
+
+}
